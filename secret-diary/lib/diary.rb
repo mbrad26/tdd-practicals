bb059aadeb0ad96e4ab570caf0e29e823ff593ed
mbrad26 <mbrad26@gmail.com>
2020-04-21 12:00:46 +0100

    before reorganising in two classes

--- secret-diary/lib/diary.rb
+++ secret-diary/lib/diary.rb
@@ -1,17 +1,22 @@
 class Diary
+ attr_reader :entries
 
  def initialize
    @lock = true
+   @entries = []
  end
 
   def add_entry(entry)
     raise 'The secret diary is closed!' if @lock
+    @entries << entry
   end
 
   def get_entries
     raise 'The secret diary is closed!' if @lock
+    @entries.pop
   end
 
   def unlock
+    @lock = false
   end
 end
--- secret-diary/spec/diary_spec.rb
+++ secret-diary/spec/diary_spec.rb
@@ -23,18 +23,25 @@ describe Diary do
 
   describe '#unlock' do
     it { is_expected.to respond_to(:unlock) }
+  end
 
-    context 'when diary is unlocked' do
-      describe '#add_entry' do
-        it 'adds an entry to the diary' do
+  context 'when diary is unlocked' do
+    before do
+      diary.unlock
+    end
 
-        end
+    describe '#add_entry' do
+      it 'adds an entry to the diary' do
+        expect { diary.add_entry('my entry') }.to change{ subject.entries }.to include 'my entry'
+      end
+    end
+
+    describe '#get_entries' do
+      it 'gets all the entries in the diary' do
+        diary.add_entry('my entry')
+
+        expect(diary.get_entries).to eq 'my entry'
       end
     end
   end
 end
-  # context 'when locked' do
-  #   it 'raises an error' do
-  #
-  #   end
-  # end
