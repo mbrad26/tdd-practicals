23a6b2132ff2d7bd7d0149272131d75c7d8b0a4e
mbrad26 <mbrad26@gmail.com>
2020-04-21 10:49:09 +0100

    responds to unlock

--- secret-diary/lib/diary.rb
+++ secret-diary/lib/diary.rb
@@ -1,8 +1,17 @@
 class Diary
 
+ def initialize
+   @lock = true
+ end
+
   def add_entry(entry)
+    raise 'The secret diary is closed!' if @lock
   end
 
   def get_entries
+    raise 'The secret diary is closed!' if @lock
+  end
+
+  def unlock
   end
 end
--- secret-diary/spec/diary_spec.rb
+++ secret-diary/spec/diary_spec.rb
@@ -1,7 +1,40 @@
 require 'diary'
 
 describe Diary do
-  it { is_expected.to respond_to(:add_entry).with(1).argument }
-  it { is_expected.to respond_to(:get_entries) }
+  subject(:diary) { described_class.new }
 
+  describe '#add_entry' do
+    it { is_expected.to respond_to(:add_entry).with(1).argument }
+
+    it 'returns an error when diary is closed' do
+      error = 'The secret diary is closed!'
+      expect { diary.add_entry('entry') }.to raise_error error
+    end
+  end
+
+  describe '#get_entries' do
+    it { is_expected.to respond_to(:get_entries) }
+
+    it 'returns an error when diary is closed' do
+      error = 'The secret diary is closed!'
+      expect { diary.get_entries }.to raise_error error
+    end
+  end
+
+  describe '#unlock' do
+    it { is_expected.to respond_to(:unlock) }
+
+    context 'when diary is unlocked' do
+      describe '#add_entry' do
+        it 'adds an entry to the diary' do
+
+        end
+      end
+    end
+  end
 end
+  # context 'when locked' do
+  #   it 'raises an error' do
+  #
+  #   end
+  # end
