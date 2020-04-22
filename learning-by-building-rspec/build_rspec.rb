a4bc5c52f1368874e0f0170ffa1f0b693ce044e7
mbrad26 <mbrad26@gmail.com>
2020-04-22 12:04:36 +0100

    added exercise 2 solution

--- learning-by-building-rspec/build_rspec.rb
+++ learning-by-building-rspec/build_rspec.rb
@@ -1,4 +1,8 @@
-def expect_to_be_equal(arg1, arg2)
-  return 'Test fails! D:' unless arg1 == arg2
-  'Test passes! :)'
+class Expect
+
+  def to_be_equal(arg1, arg2)
+    return 'Test fails! D:' unless arg1 == arg2
+    'Test passes! :)'
+  end
+
 end
