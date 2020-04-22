9520c0d2a887b4db583d468115d3138e7f2bca03
Marius Brad <mbrad26@gmail.com>
2020-04-22 08:51:35 +0100

    Update board_spec.rb

--- tic-tac/spec/board_spec.rb
+++ tic-tac/spec/board_spec.rb
@@ -1,15 +1,5 @@
 require 'board'
 
-# Exercise 1
-
-# Start writing a tic-tac-toe implementation, but only the first user stories:
-
-# I can see the board on the screen
-# I can put an X on the board
-# You might need more than one class for this, but you don't need to create them all up front if you don't want to.
-
-# Remember, test behaviour, not state. Go through the example again if you're not sure.
-
 describe Board do
   subject(:board) { described_class.new}
 
