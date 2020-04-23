9b372ad83089701815bc1cc09c4aecb2b4d033bd
Marius Brad <mbrad26@gmail.com>
2020-04-23 19:37:25 +0100

    Update extract_method.rb

--- refactoring_exercises/extract_method.rb
+++ refactoring_exercises/extract_method.rb
@@ -36,6 +36,16 @@ def browser_type?(browser)
   browser.type == "Safari"
 end
 
+# def send_correct_page(browser, window)
+#   browser_type_is_safari = browser.type == "Safari"
+#   window_size_is_small = window.size < "768px"
+
+#   safari_small_message = "You are using the Safari browser in a small window."
+#   not_safari_or_big_message = "You are not using the Safari browser or have a big window."
+
+#   browser_type_is_safari && window_size_is_small ? safari_small_message : not_safari_or_big_message
+# end
+
 def send_correct_page(browser, window)
   safari_small_message = "You are using the Safari browser in a small window."
   not_safari_or_big_message = "You are not using the Safari browser or have a big window."
