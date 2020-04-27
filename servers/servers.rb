fbf5a2613fd34c4fdc49a0b12829ae013f930db7
mbrad26 <mbrad26@gmail.com>
2020-04-27 19:42:55 +0100

    server created, exercise 1

--- servers/servers.rb
+++ servers/servers.rb
@@ -1,8 +1,13 @@
 require 'socket'
 
 server = TCPServer.new(2345)
+
 socket = server.accept
+
 socket.puts 'What do you say?'
+
 they_said = socket.gets.chomp
+
 socket.puts "You said #{they_said}. Goodbye!"
+
 socket.close
