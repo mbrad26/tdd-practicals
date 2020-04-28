a935fc3310920ebabf398465ee2a2cf91fbfda04
mbrad26 <mbrad26@gmail.com>
2020-04-28 11:59:29 +0100

    implemented the client

---
+++ clients/client.rb
@@ -0,0 +1,13 @@
+require 'socket'
+
+socket = TCPSocket.new('localhost', 2345)
+
+puts socket.gets
+
+puts 'Wow what a rude server... I\'m going to say Sarah.'
+
+socket.puts 'Sarah'
+
+puts socket.gets
+
+socket.close
