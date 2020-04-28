2fe15580f24014317873ad4c13d25cf12304e68d
mbrad26 <mbrad26@gmail.com>
2020-04-28 07:50:54 +0100

    added a while loop

--- servers/servers.rb
+++ servers/servers.rb
@@ -2,12 +2,16 @@ require 'socket'
 
 server = TCPServer.new(2345)
 
-socket = server.accept
+while true do
+  socket = server.accept
 
-socket.puts 'What do you say?'
+  socket.puts 'What do you say?'
 
-they_said = socket.gets.chomp
+  they_said = socket.gets.chomp
 
-socket.puts "You said #{they_said}. Goodbye!"
+  break if they_said == 'quit'
+
+  socket.puts "You said #{they_said}. Goodbye!"
+end
 
 socket.close
